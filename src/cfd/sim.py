53898aa998cbb10af7547b286469581dd7912f53
Mohammed Amer <mohammed.amer@fujitsu.com>
2025-11-30 15:29:40 +0000

    typer

--- src/cfd/sim.py
+++ src/cfd/sim.py
@@ -1,66 +1,35 @@
 import torch
 import torch.nn.functional as F
 from torch import Tensor
-from tqdm import tqdm
-from celluloid import Camera
-import matplotlib.pyplot as plt
+from rich.progress import track
+import typer
 
-from utils import log
+from utils import log, save_gif
 
-GRID_X = 128
-GRID_Y = 128
 
-dt = 0.01
-T = 2.
 h = 1.
-nu = 1.
-Fy = 500.
-D = 1.
-
-REACTION_LAMBDA = 0.
-
-DIFF_ITER = 20
-DIV_FREE_ITER = 20
-
-DYE_RADIUS = 4
-DYE_VAL = 1.
-DYE_S = 0.01
 
 
-def save_gif(dye_sol: Tensor):
+def neumann_boundary(p: Tensor):
 
-    fig = plt.figure()
-    ax = plt.subplot()
-
-    camera = Camera(fig)
-
-    for time_step in tqdm(dye_sol):
-        time_step = time_step.transpose(1, 0)
-        ax.imshow(time_step, cmap="inferno", origin="lower")
-        camera.snap()
-
-    anim = camera.animate()
-
-    gif_path = "output/sim.gif"
-    anim.save(gif_path, writer="pillow", fps=30)
-
-
-def neumann_boundary(p):
+    gridx, gridy = p.shape[:2]
 
     p[0:1] = p[1:2]
-    p[GRID_X-2:GRID_X-1] = p[GRID_X-3:GRID_X-2]
+    p[gridx-2:gridy-1] = p[gridx-3:gridy-2]
 
     p[:, 0:1] = p[:, 1:2]
-    p[:, GRID_Y-2:GRID_Y-1] = p[:, GRID_Y-3:GRID_Y-2]
+    p[:, gridy-2:gridy-1] = p[:, gridy-3:gridy-2]
 
 
-def impermeable_boundary(u):
+def impermeable_boundary(u: Tensor):
+
+    gridx, gridy = u.shape[:2]
 
     u[0, :, 0] = 0
-    u[GRID_X-1, :, 0] = 0
+    u[gridx-1, :, 0] = 0
 
     u[:, 0, 1] = 0
-    u[:, GRID_Y-1, 1] = 0
+    u[:, gridy-1, 1] = 0
 
 
 def div(u):
@@ -70,12 +39,12 @@ def div(u):
         (u[1:-1, 2:, 1] - u[1:-1, :-2, 1])/(2*h)
 
 
-def div_free(u, p):
+def div_free(u, p, diff_iter, dt):
 
     u_div = div(u)
 
     p = F.pad(p, pad=(1, 1, 1, 1), value=0.)
-    for _ in tqdm(range(DIV_FREE_ITER)):
+    for _ in track(range(diff_iter), description="div free"):
         p[1:-1, 1:-1] = (p[2:, 1:-1] + p[:-2, 1:-1] +
                          p[1:-1, 2:] + p[1:-1, :-2]-h**2*1/dt*u_div)/4.
 
@@ -86,7 +55,7 @@ def div_free(u, p):
     return u, p
 
 
-def diffuse(phi: Tensor, kappa):
+def diffuse(phi: Tensor, kappa, dt, diff_iter):
 
     alpha = kappa*dt/h**2
 
@@ -98,7 +67,7 @@ def diffuse(phi: Tensor, kappa):
 
     phi = F.pad(phi, pad=pad, value=0.)
 
-    for _ in tqdm(range(DIFF_ITER)):
+    for _ in track(range(diff_iter), description="diffusion"):
         phi[1:-1, 1:-1] = (phi[1:-1, 1:-1] + alpha*(phi[2:, 1:-1] +
                            phi[:-2, 1:-1] +
                            phi[1:-1, 2:]+phi[1:-1, :-2]))/(1+4*alpha)
@@ -106,7 +75,9 @@ def diffuse(phi: Tensor, kappa):
     return phi[1:-1, 1:-1]
 
 
-def semi_largangian(phi: Tensor, u: Tensor, cell: Tensor):
+def semi_largangian(phi: Tensor, u: Tensor, cell: Tensor, dt: float):
+
+    gridx, gridy = cell.shape[:2]
 
     if phi.dim() < 3:
         phi = phi[..., None]
@@ -124,8 +95,8 @@ def semi_largangian(phi: Tensor, u: Tensor, cell: Tensor):
     q22[:, :, 1] += 1
 
     def query(phi, q):
-        inside = (q[..., 0] >= 0) & (q[..., 0] < GRID_X) & \
-            (q[..., 1] >= 0) & (q[..., 1] < GRID_Y)
+        inside = (q[..., 0] >= 0) & (q[..., 0] < gridx) & \
+            (q[..., 1] >= 0) & (q[..., 1] < gridy)
 
         x = q[..., 0]
         y = q[..., 1]
@@ -172,49 +143,52 @@ def semi_largangian(phi: Tensor, u: Tensor, cell: Tensor):
     return advect.squeeze()
 
 
-def init():
-    u0 = torch.zeros(size=(GRID_X, GRID_Y, 2))
-    p = torch.zeros(size=(GRID_X, GRID_Y))
+def init(gridx, gridy, Fy, dye_radius, dye_source):
+    u0 = torch.zeros(size=(gridx, gridy, 2))
+    p = torch.zeros(size=(gridx, gridy))
 
     cellx, celly = torch.meshgrid(torch.arange(
-        GRID_X), torch.arange(GRID_Y), indexing="ij")
+        gridx), torch.arange(gridy), indexing="ij")
     cell = torch.stack([cellx, celly], dim=2)
 
-    f = torch.zeros(size=(GRID_X, GRID_Y, 2))
+    f = torch.zeros(size=(gridx, gridy, 2))
     f[..., 1] = Fy
 
     dye = torch.zeros_like(p)
 
     dye_s = torch.zeros_like(p)
 
-    centerx = GRID_X // 2
-    centery = GRID_Y // 2
+    centerx = gridx // 2
+    centery = gridy // 2
 
     mask = (cell[..., 0]-centerx)**2 + \
-        (cell[..., 1] - centery)**2 <= DYE_RADIUS**2
-    dye_s[mask] = DYE_S
+        (cell[..., 1] - centery)**2 <= dye_radius**2
+    dye_s[mask] = dye_source
 
     return u0, p, cell, f, dye, dye_s
 
 
-def run():
+def run(T: float = 2., dt: float = 0.01, nu: float = 1.,
+        reaction_lambda: float = 0., dye_diff: float = 1.,
+        gridx: int = 128, gridy: int = 128, Fy: float = 500.,
+        dye_radius: float = 4., dye_src: float = 0.01, diff_iter: int = 20,):
 
-    u, p, cell, f, dye, dye_s = init()
+    u, p, cell, f, dye, dye_s = init(gridx, gridy, Fy, dye_radius, dye_src)
 
     dye_sol = [dye]
 
     time_steps = torch.arange(0, T+dt, dt)
 
-    for _ in tqdm(time_steps):
+    for _ in track(time_steps, description="cfd"):
         u = u + f*dt
-        u = diffuse(u, kappa=nu)
-        u, p = div_free(u, p)
-        u = semi_largangian(u, u, cell=cell)
-        u, p = div_free(u, p)
+        u = diffuse(u, kappa=nu, dt=dt, diff_iter=diff_iter)
+        u, p = div_free(u, p, dt=dt, diff_iter=diff_iter)
+        u = semi_largangian(u, u, cell=cell, dt=dt)
+        u, p = div_free(u, p, dt=dt, diff_iter=diff_iter)
         dye = dye + dye_s*dt
-        dye = semi_largangian(dye, u, cell=cell)
-        dye = diffuse(dye, kappa=D)
-        dye = dye / (1+REACTION_LAMBDA*dt)
+        dye = semi_largangian(dye, u, cell=cell, dt=dt)
+        dye = diffuse(dye, kappa=dye_diff, dt=dt, diff_iter=diff_iter)
+        dye = dye / (1+reaction_lambda*dt)
 
         impermeable_boundary(u)
         neumann_boundary(p)
@@ -229,4 +203,4 @@ def run():
 
 
 if __name__ == "__main__":
-    run()
+    typer.run(run)
--- src/cfd/utils.py
+++ src/cfd/utils.py
@@ -1,6 +1,31 @@
+from celluloid import Camera
+import matplotlib.pyplot as plt
+from torch import Tensor
+from tqdm import tqdm
+from rich import print
+
+
 def log(msg):
     print(msg)
 
 
 def log_tensor_stats(tensor):
     log(f"{tensor.max()=} {tensor.min()=}")
+
+
+def save_gif(dye_sol: Tensor):
+
+    fig = plt.figure()
+    ax = plt.subplot()
+
+    camera = Camera(fig)
+
+    for time_step in tqdm(dye_sol):
+        time_step = time_step.transpose(1, 0)
+        ax.imshow(time_step, cmap="inferno", origin="lower")
+        camera.snap()
+
+    anim = camera.animate()
+
+    gif_path = "output/sim.gif"
+    anim.save(gif_path, writer="pillow", fps=30)
